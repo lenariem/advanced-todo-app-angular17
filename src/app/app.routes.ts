5626ef8dd70eb049043b403badc001285f1f14e6
unknown <olena.denysova88@gmail.com>
2024-09-27 11:51:19 +0200

    dynamic router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,26 @@
-import { Routes } from "@angular/router";
-import { TaskComponent } from "./tasks/task/task.component";
+import { Routes } from '@angular/router';
+
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   {
-    path: 'tasks',
-    component: TaskComponent,
+    path: '',
+    component: NoTaskComponent,
   },
+  {
+    path: 'users/:userId',
+    component: UserTasksComponent,
+  },
+ /*  {
+    path: 'tasks',
+    component: TasksComponent,
+    children: [
+      {
+        path: ':userId',
+        component: UserTasksComponent,
+      },
+    ],
+  }, */
 ];
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component, computed, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { type User } from './user.model';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [RouterLink, RouterLinkActive]
 })
 export class UserComponent {
   user = input.required<User>();
