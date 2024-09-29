818d491cf5f825c7e94b4d31d1bdf3eb0b887727
unknown <olena.denysova88@gmail.com>
2024-09-29 11:44:06 +0200

    fallback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,11 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix'
+      },
+      {
         path: 'tasks',
         component: TasksComponent,
       },
@@ -24,4 +30,8 @@ export const routes: Routes = [
       },
     ],
   },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
